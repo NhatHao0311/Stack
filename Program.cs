a2e9ebd854956ad8132ac9c213780c5d6e73dae4
ACER <ACER@LAPTOP-EP6K0IRF>
2023-10-21 18:45:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Test()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Stack stack = new Stack(n);
+
+            Console.WriteLine("Nhap phan tu cho Stack ( Nhap so am de ket thuc ): ");
+            int input;
+            while (true)
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    stack.Push(input);
+                }
+            }
+
+            Console.WriteLine("Cac gia tri trong Stack la: ");
+            stack.ShowStack();
+
+            Console.WriteLine();
+            Console.WriteLine($"So gia tri trong stack la: {stack.Count()}");
+            Console.WriteLine();
+
+            if (!stack.IsEmpty())
+            {
+                int value;
+                if(stack.Peek(out value))
+                {
+                    Console.WriteLine("Top of Stack: " + value);
+                }
+            }
+            Console.WriteLine();
+
+
+            while (!stack.IsEmpty())
+            {
+                int Index;
+                stack.Pop(out Index);
+                Console.WriteLine($"Popped Stack: {Index} ");
+            }
+            Console.WriteLine("Stack is empty! ");
+          
+
+        }
+        static void Main(string[] args)
+        {
+            Test();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        private int[] stackArray;
+        private int stackMax;
+        private int stackTop;
+
+        public int this[int i]
+        {
+            get { return stackArray[i]; }
+            set { stackArray[i] = value; }
+        }
+
+        public int[] StackArray
+        {
+            get { return stackArray; }
+            set {  stackArray = value; }
+        }
+
+        public int StackMax
+        {
+            get { return stackMax; }
+            set { stackMax = value; }
+        }
+
+        public int StackTop
+        {
+            get { return stackTop; }
+            set { stackTop = value; }
+        }
+
+        public Stack()
+        {
+            stackArray = new int[10];
+        }
+
+        public Stack(int value = 0)
+        {
+            stackMax = value;
+            stackArray = new int[value];
+            stackTop = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return stackTop == -1;
+        }
+
+        public bool IsFull()
+        {
+            return stackTop == stackMax - 1;
+        }
+
+        public bool Push(int value)
+        {
+            if(IsFull())
+            {
+                return false;
+            }
+            stackTop++;
+            stackArray[stackTop] = value;
+            return true;
+        }
+
+        public bool Pop(out int value)
+        {
+            value = 0;
+            if (IsEmpty())
+            {
+                return false;
+            }
+            value = stackArray[stackTop];
+            stackTop--;
+            return true;
+        }
+
+        public bool Peek(out int value)
+        {
+            value = 0;
+            if (IsEmpty())
+            {
+                return false;
+            }
+            value = stackArray[stackTop];
+            return true;
+        }
+
+
+        public void ShowStack()
+        {
+            for(int i = 0; i < stackArray.Length; i++)
+            {
+                Console.Write(stackArray[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int Count()
+        {
+            if (IsEmpty())
+            {
+                return -1;
+            }
+            else
+            {
+                int count = 0;
+                foreach (var value in stackArray)
+                {
+                    count++;
+                }
+                return count;
+            }
+        }
+
+    }
+}
